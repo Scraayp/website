9911d0366bc63ff6142a116170e046077468f468
Scraayp <scraayp@gmail.com>
2021-07-16 15:41:54 +0200

    start but idk what im doing

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
+
   <h1 class="text-red-500">test</h1>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  
+})
 </script>
